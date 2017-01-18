d2d15c3e64f7db36604b80f7e98d353d8f13c4ac
laurarbunea <laura.r.bunea+github@gmail.com>
2017-01-19 12:22:33 +1300

    First commit to fred

--- gulpfile.js/tasks/content.js
+++ gulpfile.js/tasks/content.js
@@ -3,7 +3,8 @@ const shell = require('gulp-shell');
 const config = require('../config');
 
 gulp.task('content', ['svg'], shell.task([
-    `wrangler build ${config.paths.content} ${config.paths.www} --force`,
-    `sed -i s/UA-XXXXXXX-X/${config.values.GOOGLE_ANALYTICS}/g ${config.paths.www}/*.html`,
+    `wrangler build ${config.paths.content} ${config.paths.www} --force`
 ]));
 
+//why does this break 
+//`sed -i s/UA-XXXXXXX-X/${config.values.GOOGLE_ANALYTICS}/g ${config.paths.www}/*.html`,
